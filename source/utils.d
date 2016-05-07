63bd685ee822701204aa2e7d538fd1271667e2f8
Pctg-x8 <Syn.Tri.Naga@gmail.com>
2016-05-07 17:53:16 +0900

    renamed HexInterpret to HexColor

--- source/utils.d
+++ source/utils.d
@@ -9,5 +9,5 @@ module objectivegl.utils;
 import std.meta;
 
 /// Hex to RGBA for glClearColor/glColor4f
-alias HexInterpret(uint hex) = AliasSeq!((hex & 0xff) / 255.0f, ((hex & 0xff00) >> 8) / 255.0f,
+public alias HexColor(uint hex) = AliasSeq!((hex & 0xff) / 255.0f, ((hex & 0xff00) >> 8) / 255.0f,
 	((hex & 0xff0000) >> 16) / 255.0f, ((hex & 0xff000000) >> 24) / 255.0f);

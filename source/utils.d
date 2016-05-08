5271aae82969e257479a76a3fc2433b64d1b490b
Pctg-x8 <Syn.Tri.Naga@gmail.com>
2016-05-08 15:04:14 +0900

    exchanged bit order of HexColor

--- source/utils.d
+++ source/utils.d
@@ -8,6 +8,13 @@ module objectivegl.utils;
  
 import std.meta;
 
-/// Hex to RGBA for glClearColor/glColor4f
-public alias HexColor(uint hex) = AliasSeq!((hex & 0xff) / 255.0f, ((hex & 0xff00) >> 8) / 255.0f,
-	((hex & 0xff0000) >> 16) / 255.0f, ((hex & 0xff000000) >> 24) / 255.0f);
+/// Hex to RGBA for glClearColor/glColor4f(0xaarrggbb)
+template HexColor(uint hex)
+{
+	immutable Alpha = ((hex >> 24) & 0xff) / 255.0f;
+	immutable Red = ((hex >> 16) & 0xff) / 255.0f;
+	immutable Green = ((hex >> 8) & 0xff) / 255.0f;
+	immutable Blue = (hex & 0xff) / 255.0f;
+	
+	alias HexColor = AliasSeq!(Red, Green, Blue, Alpha);
+}
